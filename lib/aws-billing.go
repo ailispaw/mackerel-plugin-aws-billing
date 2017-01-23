0a258709674b33cce24e84a69f7c03e5243fe834
littlekbt <kr.kubota.11@gmail.com>
2017-01-23 15:08:02 +0000

    function Do

--- lib/aws-billing.go
+++ lib/aws-billing.go
@@ -1,6 +1,8 @@
 package mpawsbilling
 
 import (
+  "flag"
+  "time"
 	"bytes"
 	"encoding/json"
 	"errors"
@@ -283,4 +285,27 @@ func SendServiceMetric(optApiKey string, optServiceName string) {
 	req.Header.Add("X-Api-Key", optApiKey)
 	req.Header.Set("Content-Type", "application/json")
 	client.Do(req)
+}
+
+func Do() {
+
+  now := time.Now()
+  optDest := flag.String("dest", "", "Please Specify ServiceMetric or Host (※default Host)")
+  optApiKey := flag.String("api-key", "", "API Key must have read and write authority")
+  optServiceName := flag.String("service-name", "", "target serviceName")
+  optAccessKeyID := flag.String("access-key-id", "", "AWS Access Key ID")
+  optSecretAccessKey := flag.String("secret-access-key", "", "AWS Secret Access Key")
+  optCurrency := flag.String("currency", "USD", "Unit of currency")
+  optTarget := flag.String("target", "", "Target AWS Service. if no specific Aws Service, get metrics list from cloudwatch and draw all available metrics")
+  flag.Parse()
+
+  if now.Minute() == 0 {
+    mpawsbilling.WriteCache(*optAccessKeyID, *optSecretAccessKey, *optCurrency, *optTarget)
+  }
+
+  if *optDest == "ServiceMetric" {
+    mpawsbilling.SendServiceMetric(*optApiKey, *optServiceName)
+  } else if *optDest == "Host" {
+    mpawsbilling.OutputData()
+  }
 }
