e25163d1db7d9d051b22f8d05e73f2376107d92c
A.I <ailis@paw.zone>
2017-04-25 10:35:57 -0700

    Should be able to control outside

--- lib/aws-billing.go
+++ lib/aws-billing.go
@@ -288,7 +288,6 @@ func sendServiceMetric(optApiKey string, optServiceName string) {
 
 func Do() {
 
-	now := time.Now()
 	optDest := flag.String("dest", "", "Please Specify ServiceMetric or Host (※default Host)")
 	optApiKey := flag.String("api-key", "", "API Key must have read and write authority")
 	optServiceName := flag.String("service-name", "", "target serviceName")
@@ -298,9 +297,7 @@ func Do() {
 	optTarget := flag.String("target", "", "Target AWS Service. if no specific Aws Service, get metrics list from cloudwatch and draw all available metrics")
 	flag.Parse()
 
-	if now.Minute() == 0 {
-		writeCache(*optAccessKeyID, *optSecretAccessKey, *optCurrency, *optTarget)
-	}
+	writeCache(*optAccessKeyID, *optSecretAccessKey, *optCurrency, *optTarget)
 
 	if *optDest == "ServiceMetric" {
 		sendServiceMetric(*optApiKey, *optServiceName)
